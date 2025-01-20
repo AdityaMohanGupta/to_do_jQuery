decc62dd658b58169352c10c0b5db6b3984f5b32
Aditya Mohan Gupta <aditya2004mohan@gmail.com>
2025-01-20 15:22:46 +0530

    to do

---
+++ app.js
@@ -0,0 +1,30 @@
+$('input').keypress(function(e){
+    if(e.keyCode === 13){
+        const todotext=$(this).val();
+        if(todotext!=''){
+            $('#list').append(`<li>${todotext}</li>`)
+        }
+        $(this).val('');
+    }
+})
+
+
+$('#plus').click(function(){
+    const todotext=$('input').val();
+    if(todotext!=''){
+        $('#list').append(`<li>${todotext}</li>`)
+    }
+    $('input').val('');
+})
+
+$('li').click(function(){
+    $(this).toggleClass('lol');
+})
+
+$('#bt1').click(function(){
+    $('li').remove();
+})
+
+$('#bt2').click(function(){
+    $('.lol').remove();
+})
